dfb33fb02e2ba8d28cf86f889fb18bd1c75c6aeb
Chris Spears <zometer@gmail.com>
2022-01-15 17:51:40 -0600

    initial commit

---
+++ src/main/java/com/mpqdata/app/data/mpqdataloader/MpqdataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.mpqdata.app.data.mpqdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpqdataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpqdataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpqdata/app/data/mpqdataloader/MpqdataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpqdata.app.data.mpqdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpqdataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
