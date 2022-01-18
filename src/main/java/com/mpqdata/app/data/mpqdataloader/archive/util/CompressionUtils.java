966a0dc0aad3d83a372b8cb93b1bab939bfa7eff
Chris Spears <zometer@gmail.com>
2022-01-17 21:34:31 -0600

    Adding LZ4 compression support

--- src/main/java/com/mpqdata/app/data/mpqdataloader/archive/util/CompressionUtils.java
+++ src/main/java/com/mpqdata/app/data/mpqdataloader/archive/util/CompressionUtils.java
@@ -4,16 +4,24 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.github.luben.zstd.ZstdDirectBufferDecompressingStream;
 import com.mpqdata.app.data.mpqdataloader.archive.MpqSarExtractorExcpetion;
 
+import net.jpountz.lz4.LZ4Factory;
+import net.jpountz.lz4.LZ4FastDecompressor;
+
 public class CompressionUtils {
+
+	private static Logger logger = LoggerFactory.getLogger(CompressionUtils.class);
 
 	public static final int COMPRESSION_TYPE_ZSTD = 0x4454535A;
 	public static final int COMPRESSION_TYPE_LZ4C = 0x43345A4C;
 
 	public static byte[] decompressZstd(byte[] compressedBytes, int compressedSize, byte[] dictionaryBytes) {
-		System.out.println("zstd compression");
+		logger.trace("zstd compression");
 		ByteBuffer compressedBytesBuffer = ByteBufferFactory.wrap(compressedBytes);
 
 		int dwHeaderSize = 8;
@@ -47,5 +55,33 @@ public class CompressionUtils {
 	public static byte[] decompressZstd(byte[] compressedBytes, int compressedSize) {
 		return decompressZstd(compressedBytes, compressedSize, null);
 	}
+
+	public static byte[] decompressLz4(byte[] compressedBytes, int compressedSize) {
+		logger.trace("lz4 compression");
+		ByteBuffer compressedBuffer = ByteBufferFactory.wrap(compressedBytes);
+		compressedBuffer.flip();
+
+		int headerSize = 8;
+		@SuppressWarnings("unused")
+		int signature = compressedBuffer.getInt(); //LZ4C
+		int size = compressedBuffer.getInt();
+
+		// Compressed Data body
+		byte[] bytesToExpand = Arrays.copyOfRange(compressedBytes, headerSize, compressedSize - headerSize);
+
+		LZ4Factory factory = LZ4Factory.fastestInstance();
+		LZ4FastDecompressor decompressor = factory.fastDecompressor();
+		byte[] restored = new byte[size];
+		int numBytesRead = decompressor.decompress(bytesToExpand, 0, restored, 0, size);
+		// compressedLength == compressedLength2
+
+		if (numBytesRead != compressedSize) {
+			throw new MpqSarExtractorExcpetion("compressed length from LZ4Decompressor (" + numBytesRead + ") does not match the given compressed length (" + compressedSize + ")");
+		}
+
+		return restored;
+
+	}
+
 
 }
