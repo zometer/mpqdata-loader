32fb95af808d1a9b930472ea4f2f7e4266c2c44b
Chris Spears <zometer@gmail.com>
2022-01-27 00:05:16 -0600

    #3: Adding AbilityRepository.deleteByMpqCharacterId()

--- src/main/java/com/mpqdata/app/data/mpqdataloader/model/repository/AbilityRepository.java
+++ src/main/java/com/mpqdata/app/data/mpqdataloader/model/repository/AbilityRepository.java
@@ -1,6 +1,9 @@
 package com.mpqdata.app.data.mpqdataloader.model.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import com.mpqdata.app.data.mpqdataloader.model.domain.Ability;
@@ -8,6 +11,8 @@ import com.mpqdata.app.data.mpqdataloader.model.domain.Ability;
 @Repository
 public interface AbilityRepository extends JpaRepository<Ability, String>{
 
+	@Modifying
+	@Transactional
 	public void deleteByMpqCharacterId(String mpqCharacterId);
 
 }
