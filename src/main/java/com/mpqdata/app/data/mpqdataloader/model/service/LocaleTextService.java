5cd4585ca2de00161197c78e366f76cef8e7efc5
Chris Spears <zometer@gmail.com>
2022-01-28 13:31:58 -0600

    #3: Moving the @Value property setters from the model service to the CommandLineRunner that is loaded with an explicit profile, allowing the model components to load, even when the profile the are used in is not active.

--- src/main/java/com/mpqdata/app/data/mpqdataloader/model/service/LocaleTextService.java
+++ src/main/java/com/mpqdata/app/data/mpqdataloader/model/service/LocaleTextService.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.jayway.jsonpath.DocumentContext;
@@ -37,7 +36,7 @@ public class LocaleTextService {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Value("#{'${mpq.config.languages}'.split(',')}")
+	@Setter
 	private List<String> languages;
 
 	@Autowired
