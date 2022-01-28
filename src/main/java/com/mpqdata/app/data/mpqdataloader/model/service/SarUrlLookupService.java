7b3d08d5f230b43dba087ec8f7d45286f9fca830
Chris Spears <zometer@gmail.com>
2022-01-28 13:39:57 -0600

    #3: Removing unused imports

--- src/main/java/com/mpqdata/app/data/mpqdataloader/model/service/SarUrlLookupService.java
+++ src/main/java/com/mpqdata/app/data/mpqdataloader/model/service/SarUrlLookupService.java
@@ -10,7 +10,6 @@ import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
