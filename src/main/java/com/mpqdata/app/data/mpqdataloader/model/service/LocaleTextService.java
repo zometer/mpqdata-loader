f104c159fa9cc41bcf11cf8852908adb1104740c
Chris Spears <zometer@gmail.com>
2022-01-31 15:18:09 -0600

    #3: Updating LocaleTextService to check locale_patch.json if the required key is not in locale.json

--- src/main/java/com/mpqdata/app/data/mpqdataloader/model/service/LocaleTextService.java
+++ src/main/java/com/mpqdata/app/data/mpqdataloader/model/service/LocaleTextService.java
@@ -5,14 +5,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.PathNotFoundException;
 import com.mpqdata.app.data.mpqdataloader.MpqDataLoaderException;
 import com.mpqdata.app.data.mpqdataloader.model.domain.LocaleText;
 import com.mpqdata.app.data.mpqdataloader.model.repository.LocaleTextRepository;
@@ -60,20 +64,39 @@ public class LocaleTextService {
 
 	protected void loadLocaleText(String language, List<String> keys, File dataDir) {
 		File file = new File(dataDir, LANG_DIR_MAP.get(language) + "/locale.json" ) ;
+		File patchFile = new File(dataDir, LANG_DIR_MAP.get(language) + "/locale_patch.json" ) ;
 		List<LocaleText> localeTexts = new ArrayList<>();
 
 		try {
-			DocumentContext doc = JsonPath.parse(file);
 			for (String key: keys) {
-				String text = doc.read( "$." + key, String.class);
+				String text = readLocaleText(key, file, patchFile);
 				LocaleText localeText = new LocaleText(key, language, text);
 				localeTexts.add(localeText);
 			}
 
 			localeTextRepository.saveAll(localeTexts);
+		} catch (PathNotFoundException e) {
+
 		} catch (IOException e) {
 			throw new MpqDataLoaderException(e);
 		}
+	}
+
+	protected String readLocaleText(String key, File...files) throws IOException {
+		Configuration config = Configuration.defaultConfiguration().addOptions(Option.SUPPRESS_EXCEPTIONS);
+		for (File file: files) {
+			if (! file.exists()) {
+				continue;
+			}
+			DocumentContext doc = JsonPath.using(config).parse(file);
+			String text = doc.read( "$." + key, String.class);
+			if (text != null) {
+				return text;
+			}
+		}
+
+		List<String> fileList = Stream.of(files).map(f -> f.getAbsolutePath()).toList();
+		throw new MpqDataLoaderException("Locale Key [" + key +"] not found in: " + fileList);
 	}
 
 
